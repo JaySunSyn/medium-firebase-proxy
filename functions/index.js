3d7d845643c9e2800b1b90063fbbcff31e3237ab
JaySunSyn <jfathi.mail@gmail.com>
2018-01-22 19:42:39 +0100

    minor fix

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const cors = require('cors')({origin: true});
 exports.medium = functions.https.onRequest((request, response) => {
     cors(request, response, () => {});
     const username = 'YOUR_MEDIUM_USERNAME';
-    const url = 'https://medium.com/@"+username+"/latest?format=json';
+    const url = `https://medium.com/@${username}/latest?format=json`;
     
     return rp(url)
         .then((htmlString) => {
